e892ad03fe55dc4d9517ce37bcdf70e4529a276e
Ryan Anderson <sonorousduck@gmail.com>
2022-01-22 18:47:25 -0700

    Changed some wording in the About page

--- src/Screens/AboutMeScreen.js
+++ src/Screens/AboutMeScreen.js
@@ -11,13 +11,13 @@ const About = () => {
 
         <div style={{ width: '50%', marginTop: 16, marginBottom: 16 }}>
 
-            <Typography style={{paddingLeft: 8, paddingRight: 8}} >My name is Ryan Anderson and I am currently a dual major of Computational Math and Computer Science. I am a senior currently
-                planning on pursuing a Master's degree in computer science, particulary studying Artificial Intelligence and Robotics.
+            <Typography style={{paddingLeft: 8, paddingRight: 8}} >My name is Ryan Anderson and I am a senior at Utah State University currently working towards a dual major in Computational Mathematics and Computer Science. 
+            Afterwards, I plan to pursue a Master's degree in Computer Science specializing in Artificial Intelligence and Robotics.
             </Typography>
             <br/>
 
             <Typography style={{paddingLeft: 8, paddingRight: 8}}> 
-                Additionally, I currently work as a Software Engineer Intern at Juniper System. On a daily basis, I work with C#, .NET, Blazor, and Xamarin. 
+                Additionally, I work as a Software Engineer Intern at Juniper Systems. On a daily basis, I work with C#, .NET, Blazor, and Xamarin. 
             </Typography>
 
         </div>
