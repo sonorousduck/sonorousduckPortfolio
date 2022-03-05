df1b3cf9e59b7e69b176ed17389553b7b8ce48af
Ryan Anderson <sonorousduck@gmail.com>
2022-03-04 22:12:47 -0700

    Actually updated my website. Now it looks good

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -16,15 +16,17 @@ const Home = () => {
       <Typography marginBottom={4}>
         My current projects are below! Check out my projects page for more, or go straight to my GitHub!
       </Typography>
-
+      <div className="centerChildren">
         <div className="flex-format">
             <ProjectsPost title="Coop Habit Tracker" image={require('../static/images/CoopHabitScreen.png')} alt="Coop Habit Screen Preview"
             description="Currently working on a habit creation and tracking app, that you can invite others to join in with you"
-            link="https://github.com/sonorousduck/CoopHabitTracker" />
-            <ProjectsPost title="Coop Habit Tracker" image={require('../static/images/CoopHabitScreen.png')} alt="Coop Habit Screen Preview"
-            description="Currently working on a habit creation and tracking app, that you can invite others to join in with you"
-            link="https://github.com/sonorousduck/CoopHabitTracker" />
-        </div>
+            link="https://github.com/sonorousduck/CoopHabitTracker" margin={8}/>
+            <ProjectsPost title="Calorie Tracking App" image={require('../static/images/CalorieTracker.png')} alt="Calorie Tracking Screen Preview"
+            description="Currently working on an app that allows you to track calories, weights and the trends associated with them over time"
+            link="https://github.com/Serpents-of-the-East/CalorieTracker" margin={8}/>
+          </div>
+
+      </div>
     </div>
   );
 }
--- src/Screens/ProjectsScreen.js
+++ src/Screens/ProjectsScreen.js
@@ -3,11 +3,12 @@ import { Typography } from "@mui/material";
 import { Title } from "@mui/icons-material";
 import "./../static/css/ProjectsScreen.css"
 import ProjectsPost from "../Components/ProjectsPost";
+import AudioPost from "../Components/AudioPost";
 
 const data = [
   {
       title: "Hacking Challenge",
-      // image: require("../static/images/watertracker.jpg"),
+      image: require("../static/images/HackingChallenge.png"),
       alt: "Hacking Challenge",
       description: "There was a need to develop a Hacking Challenge while I was a TA for the Web Development class at \
       Utah State University. Starting in the Summer of 2021, I developed from scratch this hacking challenge in Django, Vue and \
@@ -17,40 +18,40 @@ const data = [
     
     {
       title: "Portfolio Website",
-      // image: require("../static/images/mazegame.jpg"),
-      alt: "",
+      image: require("../static/images/PortfolioWebsite.png"),
+      alt: "Portfolio Website Image",
       description: "This website is the portfolio website. Written in React and hosted using AWS",
       link: "https://github.com/sonorousduck/sonorousduckPortfolio",
     },
 
     {
       title: "Super Smash Bot and Server",
-      // image: require("../static/images/watertracker.jpg"),
+      image: require("../static/images/SuperSmash.png"),
       alt: "Super Smash Image",
       description: "A Reinforment Agent using Deep Reinforcement learning techniques was trained to beat a level one AI. \
       Additionally, a server was created to allow for async training as well to work with the emulator. \
       Used Tensorflow as the framework.",
       link: "https://github.com/sonorousduck/SuperSmashBot",
     },
-    {
-      title: "Music Generator",
-      // image: require("../static/images/mazegame.jpg"),
-      alt: "Music Generator",
-      description: "An AI trained to learn on music and output music that sounds similar. \
-      An included example of the song is included above. Used Tensorflow as the framework",
-      link: "https://github.com/sonorousduck/MusicGenerator",
-    },
+    // {
+    //   title: "Music Generator",
+    //   // image: require("../static/images/mazegame.jpg"),
+    //   alt: "Music Generator",
+    //   description: "An AI trained to learn on music and output music that sounds similar. \
+    //   An included example of the song is included above. Used Tensorflow as the framework",
+    //   link: "https://github.com/sonorousduck/MusicGenerator",
+    // },
     {
       title: "Inigo Moncroya - Robot Vision and Navigation",
-      // image: require("../static/images/mazegame.jpg"),
-      alt: "Robot Navigation",
+      image: require("../static/images/YahboomTank.png"),
+      alt: "Image of Yahboom Tank",
       description: "A Robotics system that uses OpenCV to do object detection and a trained Tensorflow model to do human detection. \
       Used a Yahboom G1 Tank for the hardware.",
       link: "https://github.com/Max-Clark/inigo-moncrowya-cs5510",
     },
     {
       title: "Flappy Bird RL",
-      // image: require("../static/images/mazegame.jpg"),
+      image: require("../static/images/FlappyBird.png"),
       alt: "Flappy Bird Picture",
       description: "Simple Reinforcement Agent built to play and beat Flappy Bird. \
       Different methods were implemented such as Actor Critic, Deep RL learning, etc. (WIP)",
@@ -66,27 +67,30 @@ const data = [
   ];
 
 
-
 const Projects = () => {
   useEffect(() => {
     document.title = "Projects"
   }, []);
 
   return (
-    <div className="center-children">
+    <div>
+      <div className="center-children">
 
-      <Typography variant="h1" marginTop={4}>
-        Previous Projects
-      </Typography>
-      <Typography marginBottom={4}>
-        My most recent project is below! Check out my project archive for more, or go straight to my GitHub!
-      </Typography>
-      <div className="flex-format">
-      {data.map(project => 
-          <ProjectsPost title={project.title} image={project.image} alt={project.alt} description={project.description} link={project.link} margin={8}/> 
-        )}
+        <Typography variant="h1" marginTop={4}>
+          Previous Projects
+        </Typography>
+        <Typography marginBottom={4}>
+          My most recent project is below! Check out my project archive for more, or go straight to my GitHub!
+        </Typography>
+        </div>
+
+        <div className="flex-format">
+          {data.map(project => 
+              <ProjectsPost title={project.title} image={project.image} alt={project.alt} description={project.description} link={project.link} margin={8}/> 
+            )}
+          <AudioPost margin={8} />
+        </div>
       </div>
-    </div>
   );
 }
 
