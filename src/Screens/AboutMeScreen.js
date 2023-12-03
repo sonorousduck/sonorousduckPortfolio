8ed1dee5dd59ff4945999a545b32db6503e918d2
Ryan Anderson <sonorousduck@gmail.com>
2023-12-02 17:03:38 -0700

    Added a bunch of projects since I first made this site. Eventually will change to make it look a lot more 'pretty'

--- src/Screens/AboutMeScreen.js
+++ src/Screens/AboutMeScreen.js
@@ -14,14 +14,10 @@ const About = () => {
 
         <div style={{ width: '50%', marginTop: 16, marginBottom: 16 }}>
 
-            <Typography style={{paddingLeft: 8, paddingRight: 8}} >My name is Ryan Anderson and I am a senior at Utah State University currently working towards a dual major in Computational Mathematics and Computer Science. 
-            Afterwards, I plan to pursue a Master's degree in Computer Science specializing in Artificial Intelligence and Robotics.
+            <Typography style={{paddingLeft: 8, paddingRight: 8}} >My name is Ryan Anderson and I am a PhD Student at Utah State University currently working towards a Concurrent Master's in Game Engine Development and AI and a PhD in Robotics and Uncertainty.
+            
             </Typography>
             <br/>
-
-            <Typography style={{paddingLeft: 8, paddingRight: 8}}> 
-                Additionally, I work as a Software Engineer Intern at Juniper Systems. On a daily basis, I work with C#, .NET, Blazor, and Xamarin. 
-            </Typography>
 
         </div>
 
--- src/Screens/ProjectsScreen.js
+++ src/Screens/ProjectsScreen.js
@@ -7,6 +7,42 @@ import AudioPost from "../Components/AudioPost";
 
 const data = [
   {
+    title: "Save Money, Get Charged: Facility-Tied Energy Management with Unknown and Unscheduled EV Charging",
+    image: require("../static/images/PEMS.png"),
+    alt: "Predictive EMS",
+    description: "Paper that was accepted into IEEE International Conference on Intelligent Transportation Systems (ITSC) 2023 in \
+    Bilbao, Spain. (Premier conference in Electric Vehicles). Paper can be found at this link \
+    (This currently is waiting to be published by IEEE). Learns to predict energy load at a facility and controls chargers \
+    based on that charge",
+    link: "https://github.com/DIRECTLab/Predictive-EMS",
+  },
+  {
+    title: "HackUSU 2023",
+    image: require("../static/images/HackUSU2023.png"),
+    alt: "HackUSU2023",
+    description: "Developed by me and two other people working on over 24 hours to build a JRPG in Unity. We ended up taking \
+    second place among hundreds of competitors in the Game Development category. Used free Final Fantasy assets, not owned by us.",
+    link: "https://github.com/Serpents-of-the-East/hackUSU-2023",
+  },
+  {
+    title: "That Makes Sense",
+    image: require("../static/images/that_makes_sense.png"),
+    alt: "That Makes Sense",
+    description: "I worked with Daniel Snow to develop about 20 out of the 30 puzzles featured in That Makes Sense. I also built \
+    out a custom file format creator for the game to develop Hex-based maps for puzzle design. I can send a copy of the code \
+    on a case by case basis, as it is a private repo. Feel free to reach out if you want to see the editor, as well as feel \
+    free to get the game!",
+  },
+  {
+    title: "HackUSU 2022",
+    image: require("../static/images/HackUSU2022.png"),
+    alt: "HackUSU2022",
+    description: "Developed by me and two other people working over 24 hours to build a game inspired by games like Jetpack Joyride. \
+    We developed a level that ended up being pretty difficult to beat, though fun. The premise of the game was the ability to flip\
+    gravity and collect powerups to get new abilities. Much of the art was designed and drawn in the 24 hours as well by myself.",
+    link: "https://github.com/Serpents-of-the-East/hack-usu",
+  },
+  {
       title: "Hacking Challenge",
       image: require("../static/images/HackingChallenge.png"),
       alt: "Hacking Challenge",
