2abf4581cc04a4af302bb0e93207ac7babdf3a16
Ryan Anderson <sonorousduck@gmail.com>
2024-01-03 11:07:22 -0700

    Added more projects. Need to still update the about me page, then push out a build

--- src/Screens/ProjectsScreen.js
+++ src/Screens/ProjectsScreen.js
@@ -8,22 +8,31 @@ import Navbar from "../Components/Header";
 
 const regularProjects = [
   {
-    title: "Acoustic Stealth Navigation",
-    image: require("../static/images/acoustic_stealth.png"),
-    alt: "Acoustic Stealth Preview",
-    description: "Developing an algorithm using ROS2 for determing routes of best acoustic masking for navigation",
-    disabled: true,
-    icons: ["Python", "PyTorch", "ROS2", "Unity"]
-
+    title: "Save Money, Get Charged: Facility-Tied Energy Management with Unknown and Unscheduled EV Charging",
+    image: require("../static/images/PEMS.png"),
+    alt: "Predictive EMS",
+    description: "Paper that was accepted into IEEE International Conference on Intelligent Transportation Systems (ITSC) 2023 in \
+    Bilbao, Spain. (Premier conference in Electric Vehicles). Paper can be found at this link \
+    (This currently is being published by IEEE. Link will be updated). Learns to predict energy load at a facility and controls chargers \
+    based on that charge",
+    link: "https://github.com/DIRECTLab/Predictive-EMS",
+    icons: ["Python", "PyTorch", "Javascript", "REST"]
   },
   {
     title: "Ebony",
     image: require("../static/images/Ebony_Example.png"),
     alt: "Ebony",
     description: "For my Master's thesis, I am working on an easily extensible Game Engine with support for easy Reinforcement Learning Algorithm integration",
-   link: "https://github.com/sonorousduck/Ebony",
-   icons: ["C++", "OpenGL", "PyTorch", "Python"]
-
+    link: "https://github.com/sonorousduck/Ebony",
+    icons: ["C++", "OpenGL", "PyTorch", "Python"]
+  },
+  {
+    title: "Acoustic Stealth Navigation",
+    image: require("../static/images/acoustic_stealth.png"),
+    alt: "Acoustic Stealth Preview",
+    description: "Developing an algorithm using ROS2 for determing routes of best acoustic masking for navigation",
+    disabled: true,
+    icons: ["Python", "PyTorch", "ROS2", "Unity"]
   },
   {
     title: "Human Trajectory Navigation",
@@ -32,7 +41,16 @@ const regularProjects = [
     description: "Developing algorithms to dynamically predict human trajectories and plan safe routes accomodating them",
     disabled: true,
     icons: ["Python", "PyTorch", "Gazebo", "ROS2"]
-
+  },
+  {
+    title: "BirdClef 2022",
+    image: require("../static/images/BirdClefResults.png"),
+    alt: "BirdClef Results",
+    description: "Project in graduate level Deep Learning Class. I built out several different ML structures for determining the call of endangered birds in Hawai'i\
+    I created my own CNN models, then used transfer learning using models such as ResNet152, RegNet, VGG19 along with several different forms of \
+    data augmentation, class imbalance mitigation, and so forth.",
+    link: "https://github.com/sonorousduck/STAT6685_Final_Project",
+    icons: ["Python", "PyTorch"]
   },
   {
     title: "RecolleX",
@@ -41,18 +59,6 @@ const regularProjects = [
     description: "Developing a React Native Expo application for a client enabling users to track interactions with others and remember experiences they have had",
     disabled: true,
     icons: ["Javascript", "React Native", "Expo", "Firebase"]
-
-  },
-  {
-    title: "Save Money, Get Charged: Facility-Tied Energy Management with Unknown and Unscheduled EV Charging",
-    image: require("../static/images/PEMS.png"),
-    alt: "Predictive EMS",
-    description: "Paper that was accepted into IEEE International Conference on Intelligent Transportation Systems (ITSC) 2023 in \
-    Bilbao, Spain. (Premier conference in Electric Vehicles). Paper can be found at this link \
-    (This currently is waiting to be published by IEEE). Learns to predict energy load at a facility and controls chargers \
-    based on that charge",
-    link: "https://github.com/DIRECTLab/Predictive-EMS",
-    icons: ["Python", "PyTorch", "Javascript", "REST"]
   },
   {
     title: "HackUSU 2023",
@@ -62,7 +68,6 @@ const regularProjects = [
     second place among hundreds of competitors in the Game Development category. Used free Final Fantasy assets, not owned by us.",
     link: "https://github.com/Serpents-of-the-East/hackUSU-2023",
     icons: ["C#", "Unity"]
-
   },
   {
     title: "That Makes Sense",
@@ -74,7 +79,16 @@ const regularProjects = [
     free to get the game!",
     link: "https://store.steampowered.com/app/2166500/That_Makes_Sense/",
     icons: ["C++", "Javascript", "React", "NodeJS"]
-
+  },
+  {
+    title: "That Makes Sense Map Editor",
+    image: require("../static/images/mapEditor.png"),
+    alt: "Map Editor",
+    description: "A map editor that I created for the puzzle game, That Makes Sense. This enabled both Daniel Snow and myself to \
+    quickly iterate on map ideas, along with exporting them into a file format that can be loaded into the game directly. We have plans to \
+    add capability for users to create their own maps and try them out as well using this editor.",
+    link: "https://github.com/Serpents-of-the-East/mgmr-editor",
+    icons: ["Javascript", "Tailwind"]
   },
   {
     title: "HackUSU 2022",
@@ -85,7 +99,26 @@ const regularProjects = [
     gravity and collect powerups to get new abilities. Much of the art was designed and drawn in the 24 hours as well by myself.",
     link: "https://github.com/Serpents-of-the-East/hack-usu",
     icons: ["c#", "Unity"]
-
+  },
+  {
+    title: "Charging Reservation Application",
+    image: require("../static/images/chargingReservationExample.png"),
+    alt: "Charging Reservation",
+    description: "I created a frontend and backend for a Mobile-Friendly web application that allows users to reserve chargers at the Electric Vehicle Roadway at ASPIRE. \
+    This allows us to better plan for user behavior and load times at the facility, as well as begin to understand high usage times. This then lets us load balance our power \
+    usage at the EVR much more efficiently.",
+    icons: ["Javascript", "Tailwind", "DaisyUI", "PostgresDB", "NodeJS", "REST"],
+    disabled: true
+  },
+  {
+    title: "ASPIRE Visualization Website and Database",
+    image: require("../static/images/visualizationWebsite.png"),
+    alt: "ASPIRE data visulization",
+    description: "I created a visualization website for a backend database (created in PostgresDB and Sequelize ORM) that I also created. \
+    This visualization server hosts data for all of UTA bus systems, UTA and ASPIRE chargers across Salt Lake City and Ogden, and provides convenient \
+    documentation using Postman for researchers to query data and reports to be generated.",
+    icons: ["Python", "Javascript", "Sequelize", "Postgres", "REST", "NodeJS", "DaisyUI"],
+    disabled: true
   },
   {
     title: "Hacking Challenge",
@@ -95,8 +128,7 @@ const regularProjects = [
       Utah State University. Starting in the Summer of 2021, I developed from scratch this hacking challenge in Django, Vue and \
       used Nginx to serve the website. I deployed it to Digital Ocean to host the server itself. Hosted at aggiectf.tk.",
     link: "https://github.com/sonorousduck/hacking_challenge",
-    icons: ["Python", "Django", "Javascript", "Vue", "Digital Ocean"]
-
+    icons: ["Python", "Django", "Javascript", "Vue", "Digital Ocean", "REST"]
   },
 
   {
@@ -105,10 +137,8 @@ const regularProjects = [
     alt: "Portfolio Website Image",
     description: "This website is the portfolio website. Written in React and hosted using AWS",
     link: "https://github.com/sonorousduck/sonorousduckPortfolio",
-    icons: ["Javascript", "React", "tailwind", "DaisyUI", "AWS"]
-
+    icons: ["Javascript", "React", "Tailwind", "DaisyUI", "AWS"]
   },
-
   {
     title: "Super Smash Bot and Server",
     image: require("../static/images/SuperSmash.png"),
@@ -117,18 +147,8 @@ const regularProjects = [
       Additionally, a server was created to allow for async training as well to work with the emulator. \n\n \
       Used Tensorflow as the framework.",
     link: "https://github.com/sonorousduck/SuperSmashBot",
-    // icons: [<FontAwesomeIcon icon={faPython} className="size-8" />, <FontAwesomeIcon icon={faJava} className="size-8" />, <FontAwesomeIcon icon={faJs} className="size-8" />]
     icons: ["Python", "Tensorflow", "Django", "REST"]
-
   },
-  // {
-  //   title: "Music Generator",
-  //   // image: require("../static/images/mazegame.jpg"),
-  //   alt: "Music Generator",
-  //   description: "An AI trained to learn on music and output music that sounds similar. \
-  //   An included example of the song is included above. Used Tensorflow as the framework",
-  //   link: "https://github.com/sonorousduck/MusicGenerator",
-  // },
   {
     title: "Inigo Moncroya - Robot Vision and Navigation",
     image: require("../static/images/YahboomTank.png"),
@@ -137,7 +157,6 @@ const regularProjects = [
       Used a Yahboom G1 Tank for the hardware.",
     link: "https://github.com/Max-Clark/inigo-moncrowya-cs5510",
     icons: ["Python", "Tensorflow", "ROS1", "OpenCV"]
-
   },
   {
     title: "Flappy Bird RL",
@@ -147,15 +166,7 @@ const regularProjects = [
       Different methods were implemented such as Actor Critic, Deep RL learning, etc. (WIP)",
     link: "https://github.com/sonorousduck/FlappyBirdRL",
     icons: ["Python", "PyTorch"]
-
   },
-  // {
-  //   title: "",
-  //   // image: require("../static/images/mazegame.jpg"),
-  //   alt: "",
-  //   description: "",
-  //   link: "",
-  // },
 ];
 
 const audioProjects = [
@@ -183,11 +194,11 @@ const Projects = () => {
         <div className="mb-8">
           <h1 className="font-sans font-semibold text-5xl">Projects</h1>
         </div>
-        {regularProjects.map((project, key) => 
-          <ProjectsPost key={key} title={project.title} image={project.image} alt={project.alt} description={project.description} link={project.link} icons={project.icons} disabled={project.disabled}/>
+        {regularProjects.map((project, key) =>
+          <ProjectsPost key={key} title={project.title} image={project.image} alt={project.alt} description={project.description} link={project.link} icons={project.icons} disabled={project.disabled} />
         )}
-        {audioProjects.map((project, key) => 
-          <AudioPost key={key} title={project.title} audioSrc={project.audioSrc} description={project.description} link={project.link} icons={project.icons}/>
+        {audioProjects.map((project, key) =>
+          <AudioPost key={key} title={project.title} audioSrc={project.audioSrc} description={project.description} link={project.link} icons={project.icons} />
         )}
 
       </div>
