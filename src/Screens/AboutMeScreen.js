d8878540488d2ff9b0813675fd002f4413ed4fee
Ryan Anderson <sonorousduck@gmail.com>
2024-01-08 11:30:07 -0700

    Added the papers I have written or worked on

--- src/Screens/AboutMeScreen.js
+++ src/Screens/AboutMeScreen.js
@@ -26,7 +26,7 @@ const About = () => {
                     <FontAwesomeIcon icon={faDownload} />   CV
                     </a>
                 </div>
-                <div style={{ width: '50%', marginTop: 16, marginBottom: 16 }}>
+                <div className=" w-1/2 my-12">
                     <p>
                         My name is Ryan Anderson and I am a Master's Student at Utah State University currently working on a thesis in Game Engine Development, AI, and Robotics. 
                         I have a strong foundation in various programming languages including C++, Python, C#, and Javascript. When I am not working, 
@@ -34,6 +34,18 @@ const About = () => {
                     </p>
                 </div>
 
+                <div className="mx-4">
+                    <p className="font-semibold">Papers published</p>
+                    <div className="divider"></div>
+                    <ul className="list-disc mx-4 my-4">
+                        <li><strong>Anderson, R.,</strong> Harper, M., (2023). Save Money, Get Charged: Facility-Tied Energy Management with Unknown and Unscheduled EV Charging. <em>International Conference on Intelligent Transportation Systems.</em></li>
+                        <li><strong>Anderson, R.,</strong> Anderson, T., Bailey, C., Anderson, J., Harper, M., (2023) Stealth Centric A*: Bio-Inspired Navigation for Ground Robots. <em>International Robotics Conference.</em></li>
+                        <li>Anderson, J., <strong>Anderson, R.,</strong> Anderson, T., Bailey, C., & Harper, M. (2023). Stealth Centric Autonomous Robot Simulator (SCARS). <em>Software Impacts,</em> 16, 100497.</li>
+                        <li><strong>Anderson, R.,</strong> Anderson, J., Anderson, T., & Harper, M. (2023). Charger Reservation Web Application. <em>Software Impacts,</em> 18, 100589.</li>
+                        <li><strong>Anderson, R.,</strong> Anderson, T., & Harper, M. (2022). Power and transportation collection and visualization. <em>Software Impacts,</em> 14, 100386.</li>
+                    </ul>
+
+                </div>
 
                
 
